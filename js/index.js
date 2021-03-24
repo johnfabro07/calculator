cedf89071ca4be4decfcbfd99f57c80314a086d3
nanashi <jfabro3124@gmail.com>
2021-03-24 12:14:57 +0800

    added keyboard functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 const screen = document.getElementById('screen');
-const equalButton = document.getElementById('equal');
-const clearButton = document.getElementById('clear');
-const changeSignButton = document.getElementById('sign');
-const dotButton = document.getElementById('dot');
-const deleteButton = document.getElementById('delete');
-const percentageButton = document.getElementById('percentage');
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
-console.log(numberButtons);
+const otherButtons = document.querySelectorAll("[data-function]");
+
 let calculator = {
     input1: null,
     input2: null,
@@ -16,65 +11,96 @@ let calculator = {
     //previous[input1, input2]
     previousInput: [null, null]
 }
+
 let {input1, input2, operator, answer, previousInput} = calculator
 let firstInputDone = false;
 let secondInputDone = false;
 let operationDone = false;
 let digitLimit = 0;
 
+window.addEventListener('keydown', function(e) {
+    const number = document.querySelector(`button[id="${e.key}"]`);
+    const currentOperator = document.querySelector(`button[data-operator="${e.key}"]`);
+    if (e.key >= 0 || e.key < 10) inputNumber(number);
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        inputOperator(currentOperator);
+    }
+    if (e.key === 'Enter' || e.key === '=') equalFunction();
+    if (e.key === 'c') changeSign();
+    if (e.key === '.') insertDot();
+    if (e.key === '%') percentage();
+    if (e.key === 'Backspace') deleteNumber();
+    if (e.key === 'Delete') clear();
+})
+
 numberButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        //condition that counts the number of times a number button is clicked 
-        //to limit input
-        if (digitLimit < 9) {
-            if (parseFloat(screen.textContent) === 0) {
-            //prevents incrementing digitLimit if number button 0 is clicked as starting input
-                if (parseFloat(screen.textContent) === parseFloat(button.id)) {
-                    digitLimit = -1;
-                };
-                inputFromZero(button)
-            //ables user to input second operand or input 2    
-            } else if (firstInputDone && input1 !== null) {
-                secondInputInitializer()
-                screen.textContent = button.id; 
-            //ables user to input new set of operations after the previous one
-            } else if(operationDone && secondInputDone === false) {
-                reset();
-                screen.textContent = button.id;
-            } else {
-                screen.textContent += button.id;
-            }
-            digitLimit++;
-        }
-    })
+    button.addEventListener('click', e => inputNumber(e.target));
 });
 
 operatorButtons.forEach(operatorButton => {
-    operatorButton.addEventListener('click', function() {
-        //uses previous answer as input1 for consecutive operations
-        if (answer !== null) {
-            input1 = answer;
-        //executes the previous operation and uses the answer as input1
-        } else if (secondInputDone && answer === null) {
-            input2 = parseFloat(screen.textContent);
-            equate(input1, input2, operator);
-            reinitializer();
-        //captures the screen and uses it as input1
-        } else {
-            input1 = parseFloat(screen.textContent);
-            previousInput[0] = input1;
-        }  
-        operator = operatorButton.id;
-        firstInputDone = true;
-        digitLimit = 0;
-    });
+    operatorButton.addEventListener('click', e => inputOperator(e.target));
 });
 
-equalButton.addEventListener('click', function() {
+otherButtons.forEach(button => {
+    button.addEventListener('click', function(e) {
+        if (e.target.id === "equal") equalFunction();
+        if (e.target.id === "sign") changeSign();
+        if (e.target.id === "dot") insertDot();
+        if (e.target.id === "percentage") percentage();
+        if (e.target.id === "clear") clear();
+    })
+});
+
+function inputNumber(button) {
+    //condition that counts the number of times a number button is clicked 
+    //to limit input
+    if (digitLimit < 9) {
+        if (parseFloat(screen.textContent) === 0) {
+        //prevents incrementing digitLimit if number button 0 is clicked as starting input
+            if (parseFloat(screen.textContent) === parseFloat(button.id)) {
+                digitLimit = -1;
+            };
+            inputFromZero(button)
+        //ables user to input second operand or input 2    
+        } else if (firstInputDone && input1 !== null) {
+            secondInputInitializer()
+            screen.textContent = button.id; 
+        //ables user to input new set of operations after the previous one
+        } else if(operationDone && secondInputDone === false) {
+            reset();
+            screen.textContent = button.id;
+        } else {
+            screen.textContent += button.id;
+        };
+        digitLimit++;
+    };
+};
+
+function inputOperator(operatorButton) {
+    //uses previous answer as input1 for consecutive operations
+    if (answer !== null) {
+        input1 = answer;
+    //executes the previous operation and uses the answer as input1
+    } else if (secondInputDone && answer === null) {
+        input2 = parseFloat(screen.textContent);
+        equate(input1, input2, operator);
+        reinitializer();
+    //captures the screen and uses it as input1
+    } else {
+        input1 = parseFloat(screen.textContent);
+        previousInput[0] = input1;
+    };
+    operator = operatorButton.id;
+    firstInputDone = true;
+    digitLimit = 0;
+};
+
+function equalFunction() {
     if (operator === null) {
         return null;
     } else if (answer === null && input1 !== 0 && secondInputDone === false) {
-        //captures input1 as input2 if user clicked equal button before inputting the second operand.
+        //captures input1 as input2 
+        //if user clicked equal button before inputting the second operand.
         //stores input1 to [previousInput[1]]
         if (input2 === null) {
             equate(input1, previousInput[0], operator);
@@ -84,7 +110,7 @@ equalButton.addEventListener('click', function() {
         } else {
             equate(input1, input2, operator);
             previousInput[1] = input2;
-        }
+        };
     } else {
         //prevents "0" in input1 from being captured as input2 
         if (input1 === 0 && previousInput[1] !== null) {
@@ -92,64 +118,63 @@ equalButton.addEventListener('click', function() {
         } else {
         //captures the screen and uses it as input2
             input2 = parseFloat(screen.textContent);
-        }
+        };
         equate(input1, input2, operator);  
-    }
+    };
     reinitializer(); 
-});
+};
 
-changeSignButton.addEventListener('click', function(){
+function changeSign(){
     if (screen.textContent[0] !== '-') {
         screen.textContent = '-' + screen.textContent;
     } else {
         screen.textContent = screen.textContent.slice(1, screen.textContent.length)
-    }
+    };
     if (firstInputDone && input1 !== null) {
         secondInputInitializer()
         screen.textContent = '-0';
         digitLimit = 1;
-    } 
-});
+    };
+};
 
-dotButton.addEventListener('click', function() {
+function insertDot() {
     if (digitLimit > -1 && digitLimit < 9) {
         if (!(screen.textContent.includes('.'))) {
             screen.textContent += '.';
         // increments digitLimit if user adds a decimal point while calculator screen is '0'
             if (parseFloat(screen.textContent) === 0) {
                 digitLimit = 1;
-            }
-        }
-    }
+            };
+        };
+    };
     if (firstInputDone && input1 !== null) {
         secondInputInitializer()
         screen.textContent = '0.';
         digitLimit = 1;
-    }
+    };
     if (operationDone && firstInputDone === false && secondInputDone === false) {
         reset()
         screen.textContent = '0.';
         digitLimit = 1;
-    }
-});
+    };
+};
 
-percentageButton.addEventListener('click', function(){
+function percentage(){
     let number = parseFloat(screen.textContent);
     let answer = number / 100;
     if (answer > 999999999 || answer < -999999999) {
         answer = answer.toExponential(3);
-    } else if (answer.toString().length > 5) {
-        answer = answer.toPrecision(5)
-    }
+    } else if (answer.toString().length > 8) {
+        answer = answer.toPrecision(3)
+    };
     screen.textContent = answer;
-})
+    reinitializer(); 
+};
 
-deleteButton.addEventListener('click', deleteNumber);
-
-clearButton.addEventListener('click', function() {
+function clear() {
     screen.textContent = '0';
     reset() 
-});
+};
 
 function equate(input1, input2, operator) {
     if (operator === 'add') {
@@ -163,13 +188,13 @@ function equate(input1, input2, operator) {
             answer = 'undefined';
         } else {
             answer = input1 / input2
-        }
-    }
+        };
+    };
     if (answer > 999999999 || answer < -999999999) {
         answer = answer.toExponential(3);
     } else if (answer.toString().length > 9) {
         answer = answer.toPrecision(9)
-    }
+    };
     screen.textContent = answer;
 };
 
@@ -179,12 +204,12 @@ function inputFromZero(button) {
     } else {
         screen.textContent += button.id;
     }
-}
+};
 // reinitializes the input screen for second input
 function secondInputInitializer() {
     firstInputDone = false;
     secondInputDone = true;
-}
+};
 // reinitializes the input screen after an execution of an operation 
 function reinitializer() {
     input1 = answer;
@@ -202,7 +227,7 @@ function deleteNumber() {
     if (digitLimit > 0) {
         if (!(digitsOnScreen[length - 1] === '.')) {
             digitLimit -= 1;
-        }
+        };
     };
 //deletes last item in the string
     screen.textContent = screen.textContent.toString().slice(0, -1);
@@ -210,7 +235,7 @@ function deleteNumber() {
 //prevents 0 from being recognized as raw text or input to delete
     if (parseFloat(screen.textContent) === 0) {
         digitLimit = 0;
-    }
+    };
     if (screen.textContent.length === 0) {
         screen.textContent = `0`;
     };
@@ -219,7 +244,7 @@ function deleteNumber() {
     };
     if (operationDone && secondInputDone === false) {
         reset()
-    }
+    };
 };
 
 function reset() {
